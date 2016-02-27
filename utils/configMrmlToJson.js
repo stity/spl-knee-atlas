d429ce8b7fab4adaea0789614e40007380d3800b
Valentin Demeusy <valentin.demeusy@gmail.com>
2016-02-27 01:23:54 -0500

    del : mrmlToJson directory to replace by submodule
    Signed-off-by: Valentin Demeusy <valentin.demeusy@gmail.com>

---
+++ utils/configMrmlToJson.js
@@ -0,0 +1,6 @@
+module.exports = {
+    mrmlFileLocation : "../../KneeAtlas-2015Sept-Slicer4-4Version.mrml",
+    colorTableFileLocation : "../../Data/knee.ctbl",
+    vtkFilesDirectory : "../../Data/",
+    jsonResultFileName : "atlasStructure.json"
+}
