75819d77f8ff86e39027d7f508d432b9eba36a03
Valentin Demeusy <valentin.demeusy@gmail.com>
2016-03-09 01:52:06 -0500

    eng : update
    Signed-off-by: Valentin Demeusy <valentin.demeusy@gmail.com>

--- utils/configMrmlToJson.js
+++ utils/configMrmlToJson.js
@@ -2,5 +2,17 @@ module.exports = {
     mrmlFileLocation : "../../KneeAtlas-2015Sept-Slicer4-4Version.mrml",
     colorTableFileLocation : "../../Data/knee.ctbl",
     vtkFilesDirectory : "../../Data/",
-    jsonResultFileName : "atlasStructure.json"
+    jsonResultFileName : "../atlasStructure.json",
+    header : {
+        "@type": "header",
+        "species": "human",
+        "organ": "knee",
+        "name" : "The SPL/NAC Knee Atlas",
+        "license" : "?",
+        "citation" : "?",
+        "version" : "1",
+        "contact" : "https://github.com/stity/spl-knee-atlas",
+        "comment" : "",
+        "coordinate_system" : "self defined"
+    }
 }
