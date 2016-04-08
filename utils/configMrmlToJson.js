9d7b70c7c4ef367ad3c04a7c44dfb4ccb688cc9e
Valentin Demeusy <valentin.demeusy@gmail.com>
2016-04-08 11:50:40 -0400

    enh : update
    Signed-off-by: Valentin Demeusy <valentin.demeusy@gmail.com>

--- utils/configMrmlToJson.js
+++ utils/configMrmlToJson.js
@@ -13,6 +13,13 @@ module.exports = {
         "version" : "1",
         "contact" : "https://github.com/stity/spl-knee-atlas",
         "comment" : "",
-        "coordinate_system" : "self defined"
-    }
+        "coordinate_system" : "self defined",
+        "roots" : []
+    },
+    labelMapFiles : [{ // can be a list of objects or just a name (if there is only one labelmap)
+        name : "../../Data/seg.nrrd",
+        includes : "*" // a list of label exception [1,23,854, ...] or just "*" which mean that this the default file to look in which means that there can only be only one file with "*" as includes value
+    }],
+    "backgroundImages" : ["../../Data/I.nrrd"] //can be a name or a list of names
+
 }
